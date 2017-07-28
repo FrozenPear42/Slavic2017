cf8862f99a483b66028bf6fe7358516e293ac5d6
sarcus <jeremi.wolosiuk@gmail.com>
2017-07-29 00:50:02 +0200

    blob spawner, slot, obelisk and shitty prefabs

---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour {
+
+    bool isFilled;
+    Blob component;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other) {
+        
+        if (false) {
+            PullBlob(other.gameObject);
+        }
+    }
+
+    bool IsFilled() {
+        return isFilled;
+    }
+
+    void PullBlob(GameObject blob) {
+        blob.GetComponent<Blob>().enabled = false;
+    }
+}
