55ed57c2fd40b4aa0a8373f1c949bdbf58aed931
gala377 <r.galczak@gmail.com>
2017-07-29 01:44:26 +0200

    GreenBlobSripts

---
+++ Assets/Scripts/GreenBlob.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenBlob : Blob {
+    PlayerMovement player;
+    public float range; 
+    bool followPlayer = false;
+
+	// Use this for initialization
+	protected void override Start () {
+        player = FindObjectOfType<PlayerMovement>();
+        base.isIdle = true;
+    }
+	
+	// Update is called once per frame
+	protected void override Update () {
+        if ((player.transform.position - transform.position).magnitude < range)
+        {
+            player.HasGreenFollower = true;
+            //Spawn folower for player
+            Destroy(gameObject);
+        }
+	}
+}
