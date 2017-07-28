4546145b6bd400f98b1c66c00ecc9f099de9c6d3
sarcus <jeremi.wolosiuk@gmail.com>
2017-07-29 01:56:03 +0200

    added prefabs, shitty example blobs and slot can pull

--- Assets/Scripts/PinkBlob.cs
+++ Assets/Scripts/PinkBlob.cs
@@ -2,17 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Obelisk : MonoBehaviour {
+public class PinkBlob : Blob {
 
+	// Use this for initialization
 	void Start () {
 		
 	}
-
+	
+	// Update is called once per frame
 	void Update () {
 		
 	}
-
-    public void ReportSuccess() {
-        
-    }
 }
--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -4,29 +4,68 @@ using UnityEngine;
 
 public class Slot : MonoBehaviour {
 
-    bool isFilled;
-    Blob component;
+    public GameObject replace_object;
+    public Blob wanted_component;
+    public Vector3 pulling_end_diff_vector;
+    public float pulling_duration;
 
-	void Start () {
-		
+    GameObject pulling_blob;
+    Vector3 pulling_start_position;
+    float pulling_start_time;
+
+    bool isCompleted;
+    bool isPulling;
+
+    void Start () {
+        isCompleted = false;
+        isPulling = false;
 	}
 
 	void Update () {
-		
+        if (isPulling) {
+            PullBlob();
+            if (pulling_blob.transform.position == transform.position + pulling_end_diff_vector) {
+                EndPullingBlob();
+            }
+        }
 	}
 
     void OnTriggerEnter(Collider other) {
-        
-        if (false) {
-            PullBlob(other.gameObject);
+        if (IsThisWantedBlob(other.gameObject)) {
+            SetPullingBlob(other.gameObject);
+            Debug.Log("same type");
+            GetComponent<Collider>().enabled = false;
         }
     }
 
-    bool IsFilled() {
-        return isFilled;
+    bool IsCompleted() {
+        return isCompleted;
     }
 
-    void PullBlob(GameObject blob) {
+    void SetPullingBlob(GameObject blob) {
         blob.GetComponent<Blob>().enabled = false;
+  
+        pulling_blob = blob;
+        pulling_start_position = pulling_blob.transform.position;
+        pulling_start_time = Time.time;
+        isPulling = true;
+    }
+
+    void PullBlob() {
+        pulling_blob.transform.position = Vector3.Lerp(pulling_start_position, transform.position + pulling_end_diff_vector, (Time.time - pulling_start_time) / pulling_duration);
+    }
+
+    void EndPullingBlob() {
+        isPulling = false;
+        Destroy(pulling_blob);
+        Instantiate(replace_object, transform.position, Quaternion.identity, transform);
+
+        isCompleted = true;
+        transform.parent.GetComponent<Obelisk>().ReportSuccess();
+    }
+
+    bool IsThisWantedBlob(GameObject blob) {
+        Blob comp = blob.GetComponent<Blob>();
+        return wanted_component.GetType().Equals(comp.GetType());
     }
 }
