e873532210915d4ee7616beda835173169849237
Wojciech Gruszka <wojciech@gruszka.eu>
2017-07-29 04:40:16 +0200

    Added CameraController. Working on Material.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Vector3 playerOffset;
+    public GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerOffset = player.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newOffset = player.transform.position - transform.position;
+        Vector3 delta = newOffset - playerOffset;
+        transform.position += delta;
+    }
+}
