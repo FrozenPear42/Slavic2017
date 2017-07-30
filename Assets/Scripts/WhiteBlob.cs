9ce009d5b22589f922ecca33e762fb1511d1e16b
Wojciech Gruszka <wojciech@gruszka.eu>
2017-07-30 09:19:15 +0200

    rafal sie bawi, white dziala

--- Assets/Scripts/WhiteBlob.cs
+++ Assets/Scripts/WhiteBlob.cs
@@ -6,13 +6,19 @@ public class WhiteBlob : Blob
 {
 
     public GameObject targetSlot;
+    public float pullingSpeed = 10f;
 
     // Use this for initialization
     protected override void Start()
     {
         base.Start();
         setIdle(false);
-        moveToPosition(targetSlot.transform.position, movementSpeed*10, 1f);
         GetComponent<AudioSource>().Play();
+    }
+
+    protected override void Update()
+    {
+        if(targetSlot != null)
+            transform.position = Vector3.Lerp(transform.position, targetSlot.transform.position, pullingSpeed*Time.deltaTime);        
     }
 }
