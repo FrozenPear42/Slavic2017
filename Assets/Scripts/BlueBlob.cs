6c3b2408fba5dc5fb28ead7ab71e096495abfa1a
Wojciech Gruszka <wojciech@gruszka.eu>
2017-07-30 07:30:56 +0200

    Added more SoundEffects

--- Assets/Scripts/BlueBlob.cs
+++ Assets/Scripts/BlueBlob.cs
@@ -2,54 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlueBlob : Blob {
-
+public class BlueBlob : Blob
+{
     public float player_wage;
     public float run_speed;
     public float eps = 1f;
 
     bool are_there_bad_guys = false;
 
-    override protected void Start () {
+    override protected void Start()
+    {
         base.Start();
         base.setIdle(true);
-	}
+    }
 
     public bool isGoingToBeIdle = false;
 
-	override protected void Update () {
+    override protected void Update()
+    {
         base.Update();
         Vector3 avg = GetMeanBadGuysPositon();
-        if (are_there_bad_guys) {
+        if (are_there_bad_guys)
+        {
             isGoingToBeIdle = false;
             Vector3 diff = transform.position - avg;
-            base.addForce(diff, run_speed/diff.magnitude, base.baseForce);
-            GetComponent<AudioSource>().Play(); //TODO: gdzies to trzeba by wrzucicc
+            base.addForce(diff, run_speed / diff.magnitude, base.baseForce);
+            if (!GetComponent<AudioSource>().isPlaying)
+                GetComponent<AudioSource>().Play(); //TODO: gdzies to trzeba by wrzucicc
         }
-        else if (!isGoingToBeIdle){
+        else if (!isGoingToBeIdle)
+        {
             isGoingToBeIdle = true;
             Invoke("MakeIdle", 3f);
         }
-	}
+    }
 
-    Vector3 GetMeanBadGuysPositon() {
+    Vector3 GetMeanBadGuysPositon()
+    {
         Collider[] colliders = base.getCollidersInRange();
         Vector3 average = Vector3.zero;
         Vector3 player_position = Vector3.zero;
 
         are_there_bad_guys = false;
         int num = 0;
-        foreach (Collider col in colliders) {
-            if (col.GetComponent<Player>() != null && col.GetComponent<Player>().HasGreenFollower) {
+        foreach (Collider col in colliders)
+        {
+            if (col.GetComponent<Player>() != null && col.GetComponent<Player>().HasGreenFollower)
+            {
                 player_position = col.transform.position;
                 are_there_bad_guys = true;
             }
-            
-            if (col.GetComponent<GreenBlob>() != null) {
+
+            if (col.GetComponent<GreenBlob>() != null)
+            {
                 are_there_bad_guys = true;
                 num++;
                 average += col.transform.position;
-            }  
+            }
         }
         if (!are_there_bad_guys) return Vector3.zero;
 
@@ -58,11 +67,12 @@ public class BlueBlob : Blob {
         else
             average = player_position;
 
-        average = (average + player_wage * player_position) / (player_wage+1);
+        average = (average + player_wage * player_position) / (player_wage + 1);
         return average;
     }
 
-    void MakeIdle() {
+    void MakeIdle()
+    {
         //Debug.Log("IDLE NOW");
         setIdle(true);
     }
